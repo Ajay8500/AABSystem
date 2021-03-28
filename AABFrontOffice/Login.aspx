e87315d3d3433ca4e2aa065fd36163dad1a65ffe
Ajay8500 <P2445244@my365.dmu.ac.uk>
2021-03-28 15:13:36 +0100

    Add Login and Edit Code

---
+++ AABFrontOffice/Login.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="AABFrontOffice.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="">
+<head runat="server">
+    <title></title>
+
+    <%--<script src="<%= ResolveUrl("customjs/jquery.js") %>" type="text/javascript"></script>--%>
+
+    <link href="Assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css" />
+    <%--<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />--%>
+    <link href="Assets/css/Login.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+
+    <script src="Assets/js/jquery-3.4.1.min.js"></script>
+    <script src="Assets/js/modernizr-2.8.3.js"></script>
+
+    <script>
+        $(document).ready(function () {
+            localStorage.clear();
+            sessionStorage.clear();
+        });
+
+    </script>
+
+
+    <div class="wrapper">
+
+
+        <form id="_form" runat="server">
+
+            <div class="login">
+                <img style="margin: 15px" src="Assets/images/alfalah.png" width="180px" />
+                <div class="row" style="color:red">
+                     <asp:Label ID="lblMsg" runat="server"></asp:Label>
+                </div>
+               <br />
+                <input type="email" textmode="Email" placeholder="email" runat="server" id="_txtEmail" required="required" autofocus="autofocus" tabindex="1" />
+                        <input type="password" placeholder="password" runat="server" id="_txtPassword" required="required" tabindex="2" />
+                        <button id="_btnLogin" runat="server" onserverclick="_btnLogin_ServerClick" >Login</button>
+                   <br />         
+                    <a id="_btn" runat="server" onserverclick="_btnRegister_ServerClick" >Create Account</a>
+
+
+            </div>
+        </form>
+    </div>
+
+</body>
+</html>
---
+++ AABFrontOffice/Login.aspx.cs
@@ -0,0 +1,85 @@
+﻿
+using AabCars.Classes;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AabCars
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        Dictionary<Object, Object> parameters = null;
+        string cs = ConfigurationManager.ConnectionStrings["AabCars"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserLogin(string email, string password)
+        {
+
+            try
+            {
+
+                parameters = new Dictionary<object, object>();
+                parameters.Add("@Email", email);
+                parameters.Add("@Password", password);
+
+                DataSet ds = DAL.showDataWhere(parameters, cs, "SpLogin");
+
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    UserDetail.UserId = Convert.ToInt32(ds.Tables[0].Rows[0]["userid"]);
+                    UserDetail.UserName = ds.Tables[0].Rows[0]["UserName"].ToString();
+                    UserDetail.Contact = ds.Tables[0].Rows[0]["Contact"].ToString();
+                    UserDetail.Email = ds.Tables[0].Rows[0]["Email"].ToString();
+                    UserDetail.Password = ds.Tables[0].Rows[0]["password"].ToString();
+                    UserDetail.Address = ds.Tables[0].Rows[0]["Address"].ToString();
+                    UserDetail.IsStaff =Convert.ToInt32(ds.Tables[0].Rows[0]["IsStaff"]);
+
+                    if(UserDetail.IsStaff == 0)
+                    {
+                        Response.Redirect("Home.aspx", false);
+
+                    }
+                    else
+                    {
+                        Response.Redirect("CarList.aspx", false);
+
+                    }
+                }
+
+                // when user authenticate from AD only
+                else
+                {
+                    ;
+                    //lblMsg.Text = "Email not found or changed";
+                    lblMsg.Text = "Wrong username or password";
+
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+        protected void _btnLogin_ServerClick(object sender, EventArgs e)
+        {
+            UserLogin(_txtEmail.Value, _txtPassword.Value);
+        }
+
+        protected void _btnRegister_ServerClick(object sender, EventArgs e)
+        {
+            Response.Redirect("RegisterOrLogin.aspx", false);
+        }
+    }
+}
