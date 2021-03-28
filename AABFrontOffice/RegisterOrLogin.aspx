06f43e201404401ae95d980326661faa5ca35f33
Ajay8500 <P2445244@my365.dmu.ac.uk>
2021-03-28 15:10:35 +0100

    Add Register Or Login or Edit Code

---
+++ AABFrontOffice/RegisterOrLogin.aspx
@@ -0,0 +1,110 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="RegisterOrLogin.aspx.cs" Inherits="AABFrontOffice.RegisterOrLogin" %>
+
+
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <style>
+        body {
+    font-family: "Open Sans", sans-serif;
+    height: 100vh;
+    /*background: url("../images/alfalah-back.jpg") 50% fixed;
+    background-size: cover;*/
+    /* height: 100%;*/
+    width: 100%;
+    background-image: url(../assets/images/alfalah-back2.jpg);
+    background-size: 100% auto;
+    background-repeat-y: no-repeat;
+}
+
+    </style>
+
+    <div class="b2 col-md-8 issueDetails center">
+        <div class="card shadow mb-4 center">
+            <div class="card-header">
+                <h6 class="m-0 font-weight-bold centerHead headerText">Registeration</h6>
+            </div>
+
+            <div class="row" style="color: green">
+                    <asp:Label ID="lblMsgsuccess" runat="server"></asp:Label>
+                </div>
+
+                <div class="row" style="color: red">
+                    <asp:Label ID="lblMsgfail" runat="server"></asp:Label>
+                </div>
+
+            <div class="card-body">
+                
+                <div class="row divTopMargin">
+                    <div class="col-md-6">
+                        <label>User Name</label>
+                        <input type="text" class="form-control" runat="server" id="_userId" value="0" required="required" hidden />
+
+                        <input type="text" placeholder="Name" class="form-control" runat="server" id="_txtName" required="required" autofocus="autofocus" tabindex="1" />
+                    </div>
+                    <div class="col-md-6">
+                        <label>User Email</label>
+                        <input type="email" textmode="Email" class="form-control" placeholder="Email" runat="server" id="_txtEmail" required="required" autofocus="autofocus" tabindex="2" />
+                    </div>
+                </div>
+                <div class="row divTopMargin">
+                    <div class="col-md-6">
+                        <label>Contact Number</label>
+                        <input type="text" textmode="text" class="form-control" placeholder="Contact" runat="server" id="_userContact" required="required" autofocus="autofocus" tabindex="3" />
+
+                    </div>
+                    <div class="col-md-6">
+                        <label>Password</label>
+                        <input type="password" class="form-control" placeholder="password" runat="server" id="_txtPassword" required="required" tabindex="4" />
+
+                    </div>
+                </div>
+                <div class="row divTopMargin">
+                    <div class="col-md-6">
+                        <label>Address</label>
+                        <input id="_address" runat="server" placeholder ="Address" name="Address" class="form-control _requiredInputs" type="text" autofocus="autofocus" />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row divTopMargin" id="btncreatediv">
+            <div class="col-md-3"></div>
+            <div class="col-md-6 divTopMargin-sm">
+                <button type="button" id="Register" class="form-control btn btn-success" runat="server" onserverclick="_btnRegister_ServerClick">Submit</button>
+            </div>
+
+            <div class="col-md-3 divTopMargin-sm">
+                <a  class="form-control btn btn-success login" runat="server" href="Login.aspx">Have a account: Login</a>
+            </div>
+
+            <div class="col-md-3"></div>
+        </div>
+        <br />
+    </div>
+
+
+
+    <script>
+        $(document).ready(function () {
+
+            var name = '<%=Session["UserName"] %>';
+
+            if (name != '') {
+                $('.login').addClass('d-none');
+                $('.headerText').html('User Profile');
+            }
+            else {
+                $('.login').removeClass('d-none');
+            }
+
+                
+
+        })
+
+    </script>
+
+</asp:Content>
+
+
+
---
+++ AABFrontOffice/RegisterOrLogin.aspx.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using AabCars.Classes;
+
+namespace AabCars
+{
+    public partial class RegisterOrLogin : System.Web.UI.Page
+    {
+        Dictionary<object, object> param = new Dictionary<object, object>();
+        public static string cs;
+
+        
+        
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+             cs = ConfigurationManager.ConnectionStrings["AabCars"].ConnectionString;
+
+            if (UserDetail.UserId != -1 && UserDetail.flag == false)
+            {
+                
+                GetUserData();
+                
+            }
+        }
+
+
+        public void GetUserData()
+        {
+            try
+            {
+                param.Clear();
+                param.Add("@UserID", UserDetail.UserId);
+                DataSet ds = DAL.showDataWhere(param, cs, "SpGetUserData");
+
+                if (ds.Tables[0].Rows.Count>0)
+                {
+                    _userId.Value = ds.Tables[0].Rows[0]["userid"].ToString();
+                    _txtName.Value = ds.Tables[0].Rows[0]["UserName"].ToString();
+                    _userContact.Value = ds.Tables[0].Rows[0]["Contact"].ToString();
+                    _txtEmail.Value = ds.Tables[0].Rows[0]["Email"].ToString();
+                    _txtPassword.Value = ds.Tables[0].Rows[0]["password"].ToString();
+                    _address.Value = ds.Tables[0].Rows[0]["Address"].ToString();
+
+                    UserDetail.flag = true;
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void Registeration(int id, string name,string contact, string address, string email, string password, bool istaff)
+        {
+
+            try
+            {
+                int userId;
+                param.Clear();
+                param.Add("@UserID", id);
+                param.Add("@UserName", name);
+                param.Add("@Contact", contact);
+                param.Add("@Email", email);
+                param.Add("@Address", address);
+                param.Add("@Password", password);
+                if (istaff)
+                {
+
+                    param.Add("@IsStaff", 1);
+                }
+                else
+                {
+
+                    param.Add("@IsStaff", 0);
+                }
+
+                DataSet ds = DAL.showDataWhere(param, cs, "UserRegistrationOrupdate");
+
+                UserDetail.UserId = Convert.ToInt32(ds.Tables[0].Rows[0]["userId"]);
+
+               
+
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+
+                    if (id == UserDetail.UserId)
+                    {
+                        lblMsgsuccess.Text = "Successfully Update";
+                    }
+                    else
+                    {
+                        lblMsgsuccess.Text = "Successfully Register";
+                    }
+
+                    UserDetail.UserId = Convert.ToInt32(ds.Tables[0].Rows[0]["userid"]);
+                    UserDetail.UserName = ds.Tables[0].Rows[0]["UserName"].ToString();
+                    UserDetail.Contact = ds.Tables[0].Rows[0]["Contact"].ToString();
+                    UserDetail.Contact = ds.Tables[0].Rows[0]["Email"].ToString();
+                    UserDetail.Password = ds.Tables[0].Rows[0]["password"].ToString();
+                    UserDetail.Address = ds.Tables[0].Rows[0]["Address"].ToString();
+                    UserDetail.flag = false;
+
+                    System.Threading.Thread.Sleep(5000);
+                    Response.Redirect("Home.aspx", false);
+                }
+                else
+                {
+                    lblMsgfail.Text = "Something went wrong! Please check internet";
+                }
+            
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+
+        public  void _btnRegister_ServerClick(object sender, EventArgs e)
+        {
+            
+            string Name = _txtName.Value;
+            string Contact = _userContact.Value;
+            string Email = _txtEmail.Value;
+            string Password = _txtPassword.Value;
+            string address = _address.Value;
+
+            if (Name == "" && Contact == "" && Email == "" && Password == "" && address == "")
+            {
+                lblMsgfail.Text = "Please Add All Fields";
+            }
+            else
+            {
+
+                int userId = Convert.ToInt32(_userId.Value);
+
+                Registeration(userId, Name, Contact, address, Email, Password, false);
+            }
+
+
+        }
+    }
+}
