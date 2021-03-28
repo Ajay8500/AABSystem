9ab35beeab73b12c722a4795cf10c698c4b6a4c7
Ajay8500 <P2445244@my365.dmu.ac.uk>
2021-03-28 23:33:51 +0100

    Add Classes

---
+++ AABClasses/clsCustomer.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AABClasses
+{
+    public class clsCustomer
+    {
+
+        //private data member for the cutsomer id property
+        private Int32 mCustomerID;
+        //CustomerID public property
+        public Int32 CustomerID
+        {
+            get
+            {
+                //this lione of code sends data out of the property
+                return mCustomerID;
+            }
+            set
+            {
+                //this line od code allows data into the property
+                mCustomerID = value;
+            }
+        }
+
+        //private data member for active
+        private Boolean mActive;
+        //public property for active
+        public bool Active
+        {
+            get
+            {
+                //return the private data
+                return mActive;
+            }
+            set
+            {
+                //set the private data
+                mActive = value;
+            }
+        }
+
+        //private data member for first name
+        private string mCustomerFirstName;
+        //public property for first name
+        public string CustomerFirstName
+        {
+            get
+            {
+                //return the private data
+                return mCustomerFirstName;
+            }
+            set
+            {
+                //set the private data
+                mCustomerFirstName = value;
+            }
+        }
+
+        //private data member for last name
+        private string mCustomerLastName;
+        //public property for last name
+        public string CustomerLastName
+        {
+            get
+            {
+                //return the private data
+                return mCustomerLastName;
+            }
+            set
+            {
+                //set the private data
+                mCustomerLastName = value;
+            }
+        }
+
+        //private data member for email
+        private string mCustomerEmail;
+        //public property for email
+        public string CustomerEmail
+        {
+            get
+            {
+                //return the private data
+                return mCustomerEmail;
+            }
+            set
+            {
+                //set the private data
+                mCustomerEmail = value;
+            }
+        }
+
+        //private data member for tele no
+        private string mCustomerTeleNo;
+        //public property for tele no
+        public string CustomerTeleNo
+        {
+            get
+            {
+                //return the private data
+                return mCustomerTeleNo;
+            }
+            set
+            {
+                //set the private data
+                mCustomerTeleNo = value;
+            }
+        }
+
+
+        public bool Find(int CustomerID)
+        {
+            //initialise the DBConnection
+            clsDataConnection dBConnection = new clsDataConnection();
+            //add the device no parameter
+            dBConnection.AddParameter("@CustomerID", CustomerID);
+            //execute the query
+            dBConnection.Execute("sproc_tblCustomer_FilterByCustomerID");
+            //if the record was found
+            if (dBConnection.Count == 1)
+            {
+                //get the device no.
+                mCustomerID = Convert.ToInt32(dBConnection.DataTable.Rows[0]["CustomerID"]);
+                mCustomerFirstName = Convert.ToString(dBConnection.DataTable.Rows[0]["CustomerFirstName"]);
+                mCustomerLastName = Convert.ToString(dBConnection.DataTable.Rows[0]["CustomerLastName"]);
+                mCustomerEmail = Convert.ToString(dBConnection.DataTable.Rows[0]["CustomerEmail"]);
+                mCustomerTeleNo = Convert.ToString(dBConnection.DataTable.Rows[0]["CustomerTeleNo"]);
+                mActive = Convert.ToBoolean(dBConnection.DataTable.Rows[0]["Active"]);
+                return true;
+            }
+            //if no record was found
+            else
+            {
+                //return indication a problem 
+                return false;
+            }
+
+        }
+
+        public string Valid(string customerFirstName, string customerLastName, string customerEmail, string customerTeleNo)
+        {
+            //create atring vairbale tos tore the error
+            String Error = "";
+            //if the CusomterFirstName is blank
+            if (customerFirstName.Length == 0)
+            {
+                //record error
+                Error = Error + "The customer first name must not be blank : ";
+            }
+            //if the customer first name is longer than 20 characters
+            if (customerFirstName.Length > 20)
+            {
+                //record error
+                Error = Error + "The customer first name must be less than 20 characters : ";
+            }
+            //if the CusomterLastName is blank
+            if (customerLastName.Length == 0)
+            {
+                //record error
+                Error = Error + "The customer Last name must not be blank : ";
+            }
+            //if the customer last name is longer than 20 characters
+            if (customerLastName.Length > 20)
+            {
+                //record error
+                Error = Error + "The customer Last name must be less than 20 characters : ";
+            }
+            //if the CusomterEmail is blank
+            if (customerEmail.Length == 0)
+            {
+                //record error
+                Error = Error + "The email address must not be blank : ";
+            }
+            //if the customer email is longer than 50 characters
+            if (customerEmail.Length > 50)
+            {
+                //record error
+                Error = Error + "The email address must be less than 50 characters : ";
+            }
+            //if the CusomterTeleNo is blank
+            if (customerTeleNo.Length == 0)
+            {
+                //record error
+                Error = Error + "The telephone No must not be blank : ";
+            }
+            //if the customer email is longer than 15 characters
+            if (customerTeleNo.Length > 15)
+            {
+                //record error
+                Error = Error + "The telephone No must be less than 15 characters : ";
+            }
+            //return the error messages
+            return Error;
+        }
+    }
+}
---
+++ AABClasses/clsCustomerCollection.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AABClasses
+{
+    public class clsCustomerCollection
+    {
+        List<clsCustomer> mCustomerList = new List<clsCustomer>();
+        //private the data member thiscustomer
+        clsCustomer mThisCustomer = new clsCustomer();
+
+        //public property for the address list
+        public List<clsCustomer> CustomerList
+        {
+            get
+            {
+                //return the private data
+                return mCustomerList;
+            }
+            set
+            {
+                //set the private data
+                mCustomerList = value;
+            }
+        }
+
+        //public property for count
+        public int Count
+        {
+            get
+            {
+                //return the private data
+                return mCustomerList.Count;
+            }
+            set
+            {
+
+            }
+        }
+
+        public clsCustomer ThisCustomer
+        {
+            get
+            {
+                //return the private data
+                return mThisCustomer;
+            }
+            set
+            {
+                //set theprivate data
+                mThisCustomer = value;
+            }
+        }
+
+        public clsCustomerCollection()
+        {
+
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_tblCustomer_SelectAll");
+            //popilate the array list with data table 
+            PopulateArray(DB);
+
+        }
+
+        public int Add()
+        {
+            //add a new record to the database based on the values on mThisCustomer
+            //connect to the datbase 
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@CustomerFirstName", mThisCustomer.CustomerFirstName);
+            DB.AddParameter("@CustomerLastName", mThisCustomer.CustomerLastName);
+            DB.AddParameter("@CustomerEmail", mThisCustomer.CustomerEmail);
+            DB.AddParameter("@CustomerTeleNo", mThisCustomer.CustomerTeleNo);
+            DB.AddParameter("@Active", mThisCustomer.Active);
+            //execute the stored procedure returning the primary key val of hte new record 
+            return DB.Execute("sproc_tblCustomer_Insert");
+        }
+
+        public void Delete()
+        {
+            //deletes the record pointed to by thisCustomer
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@CustomerID", mThisCustomer.CustomerID);
+            //execute the stored procedure
+            DB.Execute("sproc_tblCustomer_Delete");
+        }
+
+        public void Update()
+        {
+            //create a connection to the database
+            clsDataConnection DB = new clsDataConnection();
+            //add the address no parameter
+            DB.AddParameter("@CustomerID", mThisCustomer.CustomerID);
+            //add the CustomerFirstName  parameter
+            DB.AddParameter("@CustomerFirstName", mThisCustomer.CustomerFirstName);
+            //add the street parameter
+            DB.AddParameter("@CustomerLastName", mThisCustomer.CustomerLastName);
+            //add the town parameter
+            DB.AddParameter("@CustomerEmail", mThisCustomer.CustomerEmail);
+            //add the post code parameter
+            DB.AddParameter("@CustomerTeleNo", mThisCustomer.CustomerTeleNo);
+            //add the active parameter
+            DB.AddParameter("@Active", mThisCustomer.Active);
+            //execute the query
+            DB.Execute("sproc_tblCustomer_Update");
+        }
+
+        public void ReportByEmail(string CustomerEmail)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@CustomerEmail", CustomerEmail);
+            //execute stored procedure
+            DB.Execute("sproc_tblCustomer_FilterByEmail");
+            PopulateArray(DB);
+        }
+
+        void PopulateArray(clsDataConnection DB)
+        {
+            //var for the index
+            Int32 Index = 0;
+            //car to store the record count
+            Int32 RecordCount;
+            //object fro data connection
+            RecordCount = DB.Count;
+            //clear the pricate arraylist 
+            mCustomerList = new List<clsCustomer>();
+            //while threre are records to process
+            while (Index < RecordCount)
+            {
+                //create a vlank customer
+                clsCustomer ACustomer = new clsCustomer();
+                //read the fields form the current record
+                ACustomer.Active = Convert.ToBoolean(DB.DataTable.Rows[Index]["Active"]);
+                ACustomer.CustomerID = Convert.ToInt32(DB.DataTable.Rows[Index]["CustomerID"]);
+                ACustomer.CustomerFirstName = Convert.ToString(DB.DataTable.Rows[Index]["CustomerFirstName"]);
+                ACustomer.CustomerLastName = Convert.ToString(DB.DataTable.Rows[Index]["CustomerLastName"]);
+                ACustomer.CustomerEmail = Convert.ToString(DB.DataTable.Rows[Index]["CustomerEmail"]);
+                ACustomer.CustomerTeleNo = Convert.ToString(DB.DataTable.Rows[Index]["CustomerTeleNo"]);
+                //add the record to the private data member
+                mCustomerList.Add(ACustomer);
+                //point at hte nexxt record
+                Index++;
+            }
+        }
+    }
+}
